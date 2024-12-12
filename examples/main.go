fc768189c08ae07740db4d1b0702899cee78d1f0
Danil Mosolov <koti4ch@gmail.com>
2024-12-13 00:26:05 +0500

    [SB-515] chore: updated README.md

--- examples/main.go
+++ examples/main.go
@@ -10,12 +10,13 @@ import (
 // Example of usage
 func main() {
 	// Set environment variables to simulate configuration
-	_ = os.Setenv("LOG_LEVEL", "info")
-	_ = os.Setenv("LOG_PRETTY", "true")
-	_ = os.Setenv("LOG_CALLER", "true")
+	os.Setenv("LOG_LEVEL", "info")
+	os.Setenv("LOG_FORMAT", "text")
+	os.Setenv("LOG_COLOR", "true")
+	os.Setenv("LOG_CALLER", "true")
 
 	// Initialize logger with version information
-	zlog.Init("1.0.3")
+	zlog.Init()
 
 	// Log an informational message
 	log.Info().Str("foo", "bar").Msg("hello world")
