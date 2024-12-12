c7d1c52bf3766ee0cd06d3cc7a297bb377c06785
Danil Mosolov <koti4ch@gmail.com>
2024-12-12 23:11:40 +0500

    [SB-515] rework: updated Init() signature

--- zlog.go
+++ zlog.go
@@ -8,59 +8,29 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-const (
-	envLogLevel  = "LOG_LEVEL"
-	envLogPretty = "LOG_PRETTY"
-	envLogCaller = "LOG_CALLER"
-
-	defaultLogLevel = zerolog.InfoLevel // Default log level if none is set
-)
-
 // Init initializes the logger with optional configurations based on environment variables.
-func Init(version any) {
-	// Set default log level and version
-	WithLevel(defaultLogLevel)
-	WithVersion(version)
+func Init(opts ...LoggerOption) {
+	loggerOpts := defaultLoggerOptions()
+	for _, opt := range opts {
+		opt.apply(loggerOpts)
+	}
+	envLoggerOptions(loggerOpts)
 
-	// Override log level if specified in environment variable
-	if logLevel := os.Getenv(envLogLevel); logLevel != "" {
-		if level, err := zerolog.ParseLevel(logLevel); err == nil {
-			WithLevel(level)
-		} else {
-			log.Warn().Str("logLevel", logLevel).Msg("Invalid log level, using default")
-		}
+	zerolog.SetGlobalLevel(loggerOpts.level)
+
+	if loggerOpts.format == FormatModeText {
+		log.Logger = log.Logger.Output(zerolog.ConsoleWriter{ // nolint: exhaustruct
+			NoColor:    !loggerOpts.withColor,
+			Out:        os.Stderr,    // Output to stderr.
+			TimeFormat: time.RFC3339, // Time format for logging.
+		})
 	}
 
-	// Enable pretty logging if specified in environment variables
-	if os.Getenv(envLogPretty) == "true" || os.Getenv(envLogPretty) == "1" {
-		WithPretty()
+	if loggerOpts.version != nil {
+		log.Logger = log.With().Any("version", loggerOpts.version).Logger()
 	}
 
-	// Enable caller information if specified in environment variables
-	if os.Getenv(envLogCaller) == "true" || os.Getenv(envLogCaller) == "1" {
-		WithCaller()
+	if loggerOpts.withCaller {
+		log.Logger = log.With().Caller().Logger()
 	}
-}
-
-// WithLevel sets the global log level.
-func WithLevel(level zerolog.Level) {
-	zerolog.SetGlobalLevel(level)
-}
-
-// WithVersion adds the version information to the global logger.
-func WithVersion(version any) {
-	log.Logger = log.Logger.With().Any("version", version).Logger()
-}
-
-// WithPretty configures the logger to use a human-readable console output format.
-func WithPretty() {
-	log.Logger = log.Logger.Output(zerolog.ConsoleWriter{ // nolint: exhaustruct
-		Out:        os.Stdout,    // Output to standard output
-		TimeFormat: time.RFC3339, // Time format for logging
-	})
-}
-
-// WithCaller enables logging of the caller's file and line number.
-func WithCaller() {
-	log.Logger = log.With().Caller().Logger()
 }
